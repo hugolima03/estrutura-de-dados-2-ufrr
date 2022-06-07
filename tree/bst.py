6d28ec79be12c459ddd3958401c5986f5ada1131
Hugo Lima Romão <62298830+hugolima03@users.noreply.github.com>
2022-06-07 13:41:18 -0400

    deletion function

--- tree/bst.py
+++ tree/bst.py
@@ -16,6 +16,53 @@ def insert(root, value):
       root.right = insert(root.right, value)
   return root
 
+def deleteDeepest(root,d_node):
+  q = []
+  q.append(root)
+  while(len(q)):
+    temp = q.pop(0)
+    if temp is d_node:
+      temp = None
+      return
+    if temp.right:
+      if temp.right is d_node:
+        temp.right = None
+        return
+      else:
+        q.append(temp.right)
+    if temp.left:
+      if temp.left is d_node:
+        temp.left = None
+        return
+      else:
+        q.append(temp.left)
+
+def deletion(root, key):
+  if root == None :
+    return None
+  if root.left == None and root.right == None:
+    if root.key == key :
+      return None
+    else :
+      return root
+  key_node = None
+  q = []
+  q.append(root)
+  temp = None
+  while(len(q)):
+    temp = q.pop(0)
+    if temp.value == key:
+      key_node = temp
+    if temp.left:
+      q.append(temp.left)
+    if temp.right:
+      q.append(temp.right)
+  if key_node :
+    x = temp.value
+    deleteDeepest(root,temp)
+    key_node.value = x
+  return root
+
 # PERCURSOS EM PROFUNDADE (DFS)
 def inOrder(root):
   if root:
@@ -84,4 +131,5 @@ printTree(root)
 print('min: ', findMin(root))
 print('max: ', findMax(root))
 print('height: ', findHeight(root))
+deletion(root, 35)
 levelOrder(root)
