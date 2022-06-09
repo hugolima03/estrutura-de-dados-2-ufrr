1fbba44acdf2286b53e8d5727efd153afb73ee14
Hugo Lima Romão <hugo8romao@gmail.com>
2022-06-09 08:52:12 -0400

    format

--- tree/bst.py
+++ tree/bst.py
@@ -1,75 +1,90 @@
 import queue
 
+
 class Node:
-  def __init__(self, value=None):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
 
 def insert(root, value):
-  if root is None:
-    return Node(value)
-  else:
-    if value < root.value:
-      root.left = insert(root.left, value)
-    elif value > root.value:
-      root.right = insert(root.right, value)
-  return root
+    if root is None:
+        return Node(value)
+    else:
+        if value < root.value:
+            root.left = insert(root.left, value)
+        elif value > root.value:
+            root.right = insert(root.right, value)
+    return root
 
 # PERCURSOS EM PROFUNDADE (DFS)
+
+
 def inOrder(root):
-  if root:
-    inOrder(root.left)
-    print(root.value, end = " ")
-    inOrder(root.right)
+    if root:
+        inOrder(root.left)
+        print(root.value, end=" ")
+        inOrder(root.right)
+
 
 def preOrdem(root):
-  if root:
-    print(root.value)
-    preOrdem(root.left)
-    preOrdem(root.right)
+    if root:
+        print(root.value)
+        preOrdem(root.left)
+        preOrdem(root.right)
+
 
 def printTree(root, level=0):
-  if root is not None:
-    printTree(root.right, level+1)
-    print(' ' * 4 * level + '-> ' + str(root.value))
-    printTree(root.left, level+1)
+    if root is not None:
+        printTree(root.right, level+1)
+        print(' ' * 4 * level + '-> ' + str(root.value))
+        printTree(root.left, level+1)
+
 
 def findMin(root):
-  if root is None:
-    return None
-  while root.left != None:
-    root = root.left
-  return root.value
+    if root is None:
+        return None
+    while root.left != None:
+        root = root.left
+    return root.value
+
 
 def findMax(root):
-  if root is None:
-    return None
-  while root.right != None:
-    root = root.right
-  return root.value
+    if root is None:
+        return None
+    while root.right != None:
+        root = root.right
+    return root.value
+
 
 def findHeight(root):
-  if root is None:
-    return -1
-  leftH = findHeight(root.left)
-  rightH = findHeight(root.right)
-  return max(leftH, rightH) + 1
+    if root is None:
+        return -1
+    leftH = findHeight(root.left)
+    rightH = findHeight(root.right)
+    return max(leftH, rightH) + 1
+
 
 # Percurso em Level (BFS)
 q = queue.Queue()
 
-def levelOrder(root):
-  if root is None:
-    return None
-  q.put(root)
-  while not q.empty():
-    current = q.queue[0]
-    print(current.value, end=" ")
-    if current.left is not None: q.put(current.left)
-    if current.right is not None: q.put(current.right)
-    q.get()
 
+def levelOrder(root):
+    if root is None:
+        return None
+    q.put(root)
+    while not q.empty():
+        current = q.queue[0]
+        print(current.value, end=" ")
+        if current.left is not None:
+            q.put(current.left)
+        if current.right is not None:
+            q.put(current.right)
+        q.get()
+
+
+# Main
 root = None
 root = insert(root, 10)
 root = insert(root, 5)
