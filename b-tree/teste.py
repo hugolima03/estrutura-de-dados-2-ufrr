35ff6e1ddeeb57801461423ac69d8a41b2b84d59
Hugo Lima Romão <hugo8romao@gmail.com>
2022-07-05 16:43:05 -0400

    b tree insert file

--- b-tree/teste.py
+++ b-tree/teste.py
@@ -1,42 +1,42 @@
 # Searching a key on a B-tree in Python
 
+
 # Create a node
 class BTreeNode:
   def __init__(self, leaf=False):
-    self.leaf = leaf # bool: Tem nó?
+    self.leaf = leaf
     self.keys = []
     self.child = []
 
+
 # Tree
 class BTree:
-  # construtor
   def __init__(self, t):
     self.root = BTreeNode(True)
-    self.t = t # maximum degree
+    self.t = t
 
     # Insert node
   def insert(self, k):
     root = self.root
-    # rule: the max node keys length is order m - 1
-    if len(root.keys) == (2 * self.t) - 1: # case: if the new key does not fit
+    if len(root.keys) == (2 * self.t) - 1:
       temp = BTreeNode()
       self.root = temp
       temp.child.insert(0, root)
       self.split_child(temp, 0)
       self.insert_non_full(temp, k)
-    else: # case: if the new key fit, insert in order
+    else:
       self.insert_non_full(root, k)
 
-  # Insert nonfull
+    # Insert nonfull
   def insert_non_full(self, x, k):
     i = len(x.keys) - 1
-    if x.leaf: # case: if the current node is leaf
+    if x.leaf:
       x.keys.append((None, None))
       while i >= 0 and k[0] < x.keys[i][0]:
         x.keys[i + 1] = x.keys[i]
         i -= 1
       x.keys[i + 1] = k
-    else: # case: if the current node is not leaf
+    else:
       while i >= 0 and k[0] < x.keys[i][0]:
         i -= 1
       i += 1
@@ -71,29 +71,30 @@ class BTree:
         self.print_tree(i, l)
 
   # Search key in the tree
-  # search_key(4)
   def search_key(self, k, x=None):
     if x is not None:
       i = 0
       while i < len(x.keys) and k > x.keys[i][0]:
-        i += 1 # contador para percorrer as chaves
-      if i < len(x.keys) and k == x.keys[i][0]: # encotrou, retorna
+        i += 1
+      if i < len(x.keys) and k == x.keys[i][0]:
         return (x, i)
-      elif x.leaf: # Chegou no nó folha? Não encontrado
+      elif x.leaf:
         return None
-      else: # Não é folha e não encontrou? Chamada recursiva
+      else:
         return self.search_key(k, x.child[i])
+      
     else:
-      return self.search_key(k, self.root)  # Começa do root
+      return self.search_key(k, self.root)
 
 
 def main():
   B = BTree(3)
 
-  for i in range(5):
+  for i in range(10):
     B.insert((i, 2 * i))
-  
-  # B.print_tree(B.root)
+
+  B.print_tree(B.root)
+
   if B.search_key(4) is not None:
     print("\nFound")
   else:
